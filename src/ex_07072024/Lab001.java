5f43b568af553eb03bf96504e675ce655eaf5423
meghab <meghab@adobe.com>
2024-07-11 05:07:23 +0530

    All file added

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,2 +1,14 @@
-package ex_07072024;public class Lab001 {
+package ex_07072024;
+
+import java.sql.SQLOutput;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // Basic arithmetic operation
+        System.out.println(2+2);
+        System.out.println(10-2);
+        System.out.println(5*2);
+        System.out.println(100/2);
+    }
 }
